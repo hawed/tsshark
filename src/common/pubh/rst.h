619e8fed80b598eda80dfde6e42b4cadedc9c2f1
hawed <hawed@hawed.com>
2019-11-17 14:07:45 +0100

    typo

--- src/common/pubh/rst.h
+++ src/common/pubh/rst.h
@@ -14,7 +14,7 @@
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
-/
+
 #ifndef _RST_H_
 #define _RST_H_
 
